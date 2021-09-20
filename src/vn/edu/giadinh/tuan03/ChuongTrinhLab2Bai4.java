267889f37220c7339a1e6c1f40eebb4a0d5107ad
Code Guru Mouse <phuoc2311@gmail.com>
2021-09-20 09:51:34 +0700

    đang giải PT bậc I bài 4 Lab2

--- src/vn/edu/giadinh/tuan03/ChuongTrinhLab2Bai4.java
+++ src/vn/edu/giadinh/tuan03/ChuongTrinhLab2Bai4.java
@@ -28,7 +28,13 @@ public class ChuongTrinhLab2Bai4 {
     }
 
     static void giaiPTBacI(){
-        System.out.println("Triển khai code giải phương trình bậc 1 ....");
+        float a = 0, b = 0;
+        
+        if(a == 0){
+            if(b == 0){
+                System.out.println("PT vô số nghiệm...");
+            }
+        }
     }
 
     
