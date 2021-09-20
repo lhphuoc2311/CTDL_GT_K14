c9ef2984a90fdc6846c251f019769648280d5ead
Code Guru Mouse <phuoc2311@gmail.com>
2021-09-20 11:44:10 +0700

    thêm class Cow + TestDrive

---
+++ src/vn/edu/giadinh/tuan03/Cow.java
@@ -0,0 +1,21 @@
+/*
+*  crated date: Sep 20, 2021
+*  author: cgm
+*/
+package vn.edu.giadinh.tuan03;
+
+public class Cow {
+
+    double weight;
+
+    //HÀM TẠO
+    //devlopers tạo ra KHÔNG ĐỂ java compiler tạo ra
+
+    Cow(){//HÀM TẠO MẶC ĐỊNH DO developer tạo ra    
+    }
+
+    Cow(double w){
+        weight = w;
+    }
+    
+}
---
+++ src/vn/edu/giadinh/tuan03/CowTestDrive.java
@@ -0,0 +1,24 @@
+/*
+*  crated date: Sep 20, 2021
+*  author: cgm
+*/
+package vn.edu.giadinh.tuan03;
+
+public class CowTestDrive {
+
+    public static void main(String[] args) {
+        Cow cow;
+        //cow = new Cow();
+        cow = new Cow(15);
+        System.out.println("weight: " + cow.weight);
+
+
+        cow = new Cow(5);
+        System.out.println("weight: " + cow.weight);
+
+        cow.weight = 15;
+
+       
+    }
+    
+}
