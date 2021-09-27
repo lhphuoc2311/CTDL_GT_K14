125d63a359bf79fa8c63152d1b8b1cc5f057ad11
Code Guru Mouse <phuoc2311@gmail.com>
2021-09-27 11:43:28 +0700

    chuyển đổi array sang List để sắp xếp

--- src/com/phuoclh12345678/tuan04/StudentTestDrive.java
+++ src/com/phuoclh12345678/tuan04/StudentTestDrive.java
@@ -4,6 +4,9 @@
 */
 package com.phuoclh12345678.tuan04;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Scanner;
 
 public class StudentTestDrive {
@@ -11,6 +14,8 @@ public class StudentTestDrive {
     public static void main(String[] args) {
         //tạo 1 danh sách sinh viên
         Student[] studentList  = new Student[2];
+
+        
 
         //nhập danh sách sinh viên từ bàn phím
         Scanner varInput = new Scanner(System.in);
@@ -30,6 +35,30 @@ public class StudentTestDrive {
 
         //in danh sách sinh viên
         System.out.println("============in danh sách sinh viên sau nhập===========");
+        for (Student student : studentList) {
+
+            student.inThongTin();
+            
+        }
+
+        List list = Arrays.asList(studentList);
+        
+
+        //BỘ SO SÁNH
+        Comparator comparator = new Comparator<Student>(){
+
+            @Override
+            public int compare(Student o1, Student o2) {
+                
+                return Integer.compare(o1.age, o2.age);
+            }
+            
+        };
+
+        list.sort(comparator);
+
+        //in danh sách sinh viên
+        System.out.println("============in danh sách sinh viên sau khi so sánh===========");
         for (Student student : studentList) {
 
             student.inThongTin();
