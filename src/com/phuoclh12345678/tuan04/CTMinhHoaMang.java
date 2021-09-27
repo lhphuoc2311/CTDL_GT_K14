2fc681c84fcba34d8f8fe3cf28f9a367f173e50f
Code Guru Mouse <phuoc2311@gmail.com>
2021-09-27 08:11:51 +0700

    Minh họa khai báo và khởi tạo mảng

---
+++ src/com/phuoclh12345678/tuan04/CTMinhHoaMang.java
@@ -0,0 +1,35 @@
+/*
+*  created date: Sep 27, 2021
+*  author: cgm
+*/
+package com.phuoclh12345678.tuan04;
+
+import java.util.Arrays;
+
+public class CTMinhHoaMang {
+    public static void main(String[] args) {
+        //cú pháp khai báo mảng
+        int[] mangSoNguyen;//chưa chỉ định số phần tử
+
+        mangSoNguyen = new int[5];//=> mảng trong java là 1 kiểu tham chiếu đối tượng
+
+        
+        double mangSoThuc[] = new double[10];
+
+
+        //trước khi dùng thì phải xác định số phần tử của mảng
+        mangSoNguyen[0] = 2;
+
+        //KHAI BÁO CÓ KHỞI TẠO GIÁ TRỊ CHO MẢNG
+        double diemMonHoc[] = new double[]{10.5, 5.5, 3.0, 4.5};
+        System.out.println("Mảng điểm môn học: " + Arrays.toString(diemMonHoc));
+        diemMonHoc[0] = diemMonHoc[1];
+        //5.5, 5.5, 3.0, 4.5
+        
+        System.out.println("Mảng điểm môn học: " + Arrays.toString(diemMonHoc));
+
+
+        //CÁCH 2
+        String dsTenSinhVien[] = {"Tèo", "Tý", "Tửng"};
+    }
+}
